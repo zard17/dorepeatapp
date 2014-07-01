eec5a778e3f09d89e661dce571accdfb92e88a0d
Kangsan Lee <kangsan.lee@gmail.com>
2014-07-01 14:50:31 +0900

    show list and add update.tmpl.html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,13 +11,14 @@ angular.module('sampleAppApp', ['ngRoute'])
     $scope.repeat = {
       title: '',
       description: '',
-      checklist: ''
+      checklist: '',
+      show: false
     };
 
     $scope.repeat_list = [
-      {"title": "Go to a gym", "description": "exercise every morning at 8 o'clock", "checklist": ['run 10 km', 'crunch 50 times']},
-      {"title": "Study English", "description": "Study Good Morning Pops everyday", "checklist": ['listen to broadcast', 'write down essay']},
-      {"title": "Eat lunch", "description": "Eat fresh food without meat", "checklist": ['drink milk']}
+      {"title": "Go to a gym", "description": "exercise every morning at 8 o'clock", "checklist": ['run 10 km', 'crunch 50 times'], "show" : false},
+      {"title": "Study English", "description": "Study Good Morning Pops everyday", "checklist": ['listen to broadcast', 'write down essay'], "show" : false},
+      {"title": "Eat lunch", "description": "Eat fresh food without meat", "checklist": ['drink milk'], "show" : false}
       ];
     //$scope.repeat_list = list;
 
@@ -26,12 +27,17 @@ angular.module('sampleAppApp', ['ngRoute'])
       console.log($scope.repeat.description);
       console.log($scope.repeat.checklist);
       $scope.repeat_list.push($scope.repeat);
-    };
+    }
+
+    $scope.setRepeatShow = function (title, $index) {
+      console.log($index + ' ' + title + ' ' + $scope.repeat_list[$index].show);
+      $scope.repeat_list[$index].show ? $scope.repeat_list[$index].show = false : $scope.repeat_list[$index].show = true;
+    }
   })
 
   .config(function($routeProvider) {
   	$routeProvider
       .when('/my_repeats', {templateUrl: 'scripts/templates/my_repeats.tmpl.html'})
-  		.when('/new_repeat', {templateUrl: 'scripts/templates/new_repeat.tmpl.html'})
+  		.when('/create_repeat', {templateUrl: 'scripts/templates/create_repeat.tmpl.html'})
   		.otherwise({redirectTo: '/'});
   });
